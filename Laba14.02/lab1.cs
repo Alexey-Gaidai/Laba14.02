ccb1bd3d229f35c6ac1008d5dc87d5d8aaa82cb5
Alexey-Gaidai <71748686+Alexey-Gaidai@users.noreply.github.com>
2021-02-13 15:02:06 +0700

    Добавьте файлы проекта.

---
+++ Laba14.02/lab1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Laba14._02
+{
+    class lab1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            double chislo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите степень: ");
+            double stepen = Convert.ToDouble(Console.ReadLine());
+
+            double result = 1;
+
+            for (int i = 0; i < stepen; i++)
+            {
+                result *= chislo;
+            }
+
+            Console.Write("Результат: ");
+            Console.WriteLine(result);
+        }
+    }
+}
